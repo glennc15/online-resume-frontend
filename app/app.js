cdc9689e4b6ed6ac60761937556de973dfeebb91
Glenn <gcrosby15@gmail.com>
2024-03-27 18:42:55 -0500

    made template of the home view. The home view controller is passing hard coed values to the view. Will be an api call when the backend is set up

--- app/app.js
+++ app/app.js
@@ -1,51 +1,51 @@
 (function() {
 
 
-// angular.module('myLoc8rApp', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
-angular.module('myLoc8rApp', ['ngRoute', 'ngSanitize']);
+// angular.module('onlineResume', ['ngRoute', 'ngSanitize', 'ui.bootstrap']);
+angular.module('onlineResume', ['ngRoute', 'ngSanitize']);
 
 
 
 
 function config ($routeProvider, $locationProvider) {
-	
+
 	$routeProvider
 		.when('/', {
 			templateUrl: 'home/home.view.html',
 			controller: 'homeCtrl',
 			controllerAs: 'vm'
 		})
-		.when('/locations', {
-			templateUrl: 'locations/locations.view.html',
-			controller: 'locationsCtrl',
-			controllerAs: 'vm'
-		})
-		.when('/about', {
-			// templateUrl: '/common/views/genericText.view.html',
-			templateUrl: '/about/about.view.html',
-			controller: 'aboutCtrl',
-			controllerAs: 'vm'
-		})
-		.when('/location/:locationid', {
-			templateUrl: '/locationDetail/locationDetail.view.html',
-			controller: 'locationDetailCtrl',
-			controllerAs: 'vm'
-		})
-		.when('/addreview/:locationid', {
-			templateUrl: '/addReview/addReview.view.html',
-			controller: 'addReviewCtrl',
-			controllerAs: 'vm'
-		})
-		.when('/register', {
-			templateUrl: '/auth/register/register.view.html',
-			controller: 'registerCtrl',
-			controllerAs: 'vm'
-		})
-		.when('/login', {
-			templateUrl: '/auth/login/login.view.html',
-			controller: 'loginCtrl',
-			controllerAs: 'vm'
-		})
+		// .when('/locations', {
+		// 	templateUrl: 'locations/locations.view.html',
+		// 	controller: 'locationsCtrl',
+		// 	controllerAs: 'vm'
+		// })
+		// .when('/about', {
+		// 	// templateUrl: '/common/views/genericText.view.html',
+		// 	templateUrl: '/about/about.view.html',
+		// 	controller: 'aboutCtrl',
+		// 	controllerAs: 'vm'
+		// })
+		// .when('/location/:locationid', {
+		// 	templateUrl: '/locationDetail/locationDetail.view.html',
+		// 	controller: 'locationDetailCtrl',
+		// 	controllerAs: 'vm'
+		// })
+		// .when('/addreview/:locationid', {
+		// 	templateUrl: '/addReview/addReview.view.html',
+		// 	controller: 'addReviewCtrl',
+		// 	controllerAs: 'vm'
+		// })
+		// .when('/register', {
+		// 	templateUrl: '/auth/register/register.view.html',
+		// 	controller: 'registerCtrl',
+		// 	controllerAs: 'vm'
+		// })
+		// .when('/login', {
+		// 	templateUrl: '/auth/login/login.view.html',
+		// 	controller: 'loginCtrl',
+		// 	controllerAs: 'vm'
+		// })
 		.otherwise({redirectTo: '/'});
 
 	$locationProvider.html5Mode(true);
@@ -54,7 +54,7 @@ function config ($routeProvider, $locationProvider) {
 
 
 angular
-	.module('myLoc8rApp')
+	.module('onlineResume')
 	.config(['$routeProvider', '$locationProvider', config]);
 
 
--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -47,28 +47,33 @@ def test_gethub_link(page: Page):
 
 def test_data_eng_skills(page: Page):
     page.goto("http://localhost:8081")
-    expect(page.get_by_text("Data Engineering")).to_be_visible()
-    expect(page.get_by_text("Apache Airflow · AWS · Terraform")).to_be_visible()
+
+    # have to use an xpath locator because "Data Engineering" is used in several
+    # differenct places. Without xpath causes a strict mode error.
+    expect(page.locator("xpath=//section[3]//div//div[2]//div[1]//div[1][text()='Data Engineering']")).to_be_visible()
+    # expect(page.get_by_text("Data Engineering")).to_be_visible()
+
+    expect(page.get_by_text("Apache Airflow ⸱ AWS ⸱ Terraform")).to_be_visible()
 
 def test_web_scraping_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Web Scraping")).to_be_visible()
-    expect(page.get_by_text("Scrapy · Playwright · Requests · Bash")).to_be_visible()
+    expect(page.get_by_text("Scrapy ⸱ Playwright ⸱ Requests ⸱ Bash")).to_be_visible()
 
 def test_devops_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("DevOps")).to_be_visible()
-    expect(page.get_by_text("Docker · GitHub · CI/CD")).to_be_visible()
+    expect(page.get_by_text("Docker ⸱ GitHub ⸱ CI/CD")).to_be_visible()
 
 def test_web_development_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Web Development")).to_be_visible()
-    expect(page.get_by_text("Angular · HTML · CSS")).to_be_visible()
+    expect(page.get_by_text("Angular ⸱ HTML ⸱ CSS")).to_be_visible()
 
 def test_database_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Database Systems")).to_be_visible()
-    expect(page.get_by_text("MongoDB · PostgreSQL")).to_be_visible()
+    expect(page.get_by_text("MongoDB ⸱ PostgreSQL")).to_be_visible()
 
 def test_programming_languages_skills(page: Page):
     page.goto("http://localhost:8081")
@@ -78,40 +83,40 @@ def test_programming_languages_skills(page: Page):
     expect(page.locator("xpath=//section[3]/div//div[2]//div[6]//div[1][text()='Programming Languages']")).to_be_visible()
     # expect(page.get_by_text("Programming Languages")).to_be_visible()
 
-    expect(page.get_by_text("Python · JavaScript")).to_be_visible()
+    expect(page.get_by_text("Python ⸱ JavaScript")).to_be_visible()
 
 def test_operating_systems_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Operating Systems")).to_be_visible()
-    expect(page.get_by_text("Mac OS · GNU/Linux · Windows")).to_be_visible()
+    expect(page.get_by_text("Mac OS ⸱ GNU/Linux ⸱ Windows")).to_be_visible()
 
 def test_industrial_programming_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Industrial Programming")).to_be_visible()
-    expect(page.get_by_text("SCL · STL · Ladder Logic")).to_be_visible()
+    expect(page.get_by_text("SCL ⸱ STL ⸱ Ladder Logic")).to_be_visible()
 
 def test_industrial_networks_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Industrial Networks")).to_be_visible()
-    expect(page.get_by_text("Profibus DP · Modbus · Industrial Ethernet")).to_be_visible()
+    expect(page.get_by_text("Profibus DP ⸱ Modbus ⸱ Industrial Ethernet")).to_be_visible()
 
 def test_industrial_hardware_skills(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Industrial Hardware")).to_be_visible()
-    expect(page.get_by_text("Siement 300/400H · Allen Bradley PLC5/SLC · Omron")).to_be_visible()
+    expect(page.get_by_text("Siement 300/400H ⸱ Allen Bradley PLC5/SLC ⸱ Omron")).to_be_visible()
 
 def test_commissioning_manager_experience(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Installation and Commissioning Manager @ National Oilwell Varco")).to_be_visible()
     expect(page.get_by_text("China/UAE")).to_be_visible()
-    expect(page.get_by_text("Jan 2010 – Sep 2019")).to_be_visible()
+    expect(page.get_by_text("Jan 2010 - Sep 2019")).to_be_visible()
     expect(page.get_by_text("I oversaw complex projects related to the installation and commissioning of cutting-edge equipment in the oil and gas industry. My responsibilities included project planning, team leadership, and ensuring the successful integration of equipment in challenging operational environments, contributing to the company's reputation for excellence.")).to_be_visible()
 
 def test_commissioning_engineer_experience(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Commissioning Engineer @ National Oilwell Varco")).to_be_visible()
     expect(page.get_by_text("China/Korea/Singapore/UAE")).to_be_visible()
-    expect(page.get_by_text("May 2000 – Jan 2010")).to_be_visible()
+    expect(page.get_by_text("May 2000 - Jan 2010")).to_be_visible()
     expect(page.get_by_text("I specialized in overseeing the commissioning process for oil and gas equipment. My role emphasized a commitment to safety, strong troubleshooting skills, and effective communication. I thrived in remote and challenging work environments while maintaining meticulous documentation of critical processes.")).to_be_visible()
 
 def test_electrical_designer_experience(page: Page):
@@ -126,7 +131,7 @@ def test_electrical_designer_experience(page: Page):
     expect(page.locator("xpath=//div[4]//div[1]//small[text()='Houston, TX']")).to_be_visible()
     # expect(page.get_by_text("Houston, TX")).to_be_visible()
 
-    expect(page.get_by_text("May 1999 – May 2000")).to_be_visible()
+    expect(page.get_by_text("May 1999 - May 2000")).to_be_visible()
     expect(page.get_by_text("Produced detailed electrical schematics and panel assembly drawings, complete with comprehensive BOMs, using AutoCAD and Solid Edge.")).to_be_visible()
     expect(page.get_by_text("Develop custom elevator control software on Omron and Allen Bradley PLCs, along with intuitive HMI interfaces, enhancing elevator system efficiency and functionality.")).to_be_visible()
 
@@ -142,7 +147,7 @@ def test_boiler_technician_experience(page: Page):
     # expect(page.get_by_text("Houston, TX")).to_be_visible()
 
 
-    expect(page.get_by_text("Sep 1994 – May 1999")).to_be_visible()
+    expect(page.get_by_text("Sep 1994 - May 1999")).to_be_visible()
     expect(page.get_by_text("Integral member of a dynamic engineering team, actively involved in diverse engineering disciplines.")).to_be_visible()
     expect(page.get_by_text("Designed custom burner monitoring and boiler control software for Allen Bradley PLCs meeting stringent NFPA 8502 requirements.")).to_be_visible()
 
@@ -150,22 +155,22 @@ def test_navy_experience(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Electrician's Mate, Second Class @ United States Navy")).to_be_visible()
     expect(page.get_by_text("USS Sturgeon")).to_be_visible()
-    expect(page.get_by_text("Aug 1989 – Aug 1994")).to_be_visible()
+    expect(page.get_by_text("Aug 1989 - Aug 1994")).to_be_visible()
     expect(page.get_by_text("As a US Navy Nuclear-Trained Electrician on a fast attack submarine, I held a critical role in ensuring the safe and reliable operation of the ship's electrical systems, contributing to mission success and crew safety in challenging and dynamic environments.")).to_be_visible()
 
 def test_AWS_education(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("AWS Cloud Practitioner Certified")).to_be_visible()
-    expect(page.get_by_text("Mar 2023")).to_be_visible()
+    expect(page.get_by_text("Mar 2024")).to_be_visible()
 
 def test_HCC_education(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Houston Community College @ Main Campus, Houston, TX")).to_be_visible()
-    expect(page.get_by_text("Jan 1995 – Jul 1998")).to_be_visible()
+    expect(page.get_by_text("Jan 1995 - Jul 1998")).to_be_visible()
 
 
 def test_navy_education(page: Page):
     page.goto("http://localhost:8081")
     expect(page.get_by_text("Naval Nuclear Power School @ NTC Orlando, Fl")).to_be_visible()
-    expect(page.get_by_text("Jul 2000 – Dec 2000")).to_be_visible()
+    expect(page.get_by_text("Jul 1990 - Dec 1990")).to_be_visible()
 
